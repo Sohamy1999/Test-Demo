25983c954af6f9db4f9963f1241a37bc0ba62750
Yadav <soham.deepak-yadav@capgemini.com>
2022-03-03 16:48:45 +0530

    first commit

---
+++ src/main/java/SohamTest/sohamtest/App.java
@@ -0,0 +1,13 @@
+package SohamTest.sohamtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Soham" );
+    }
+}
